44456edf5fb4dbd03191dfe57a6e45ba8629228c
Laura <laura.e.vollmer@gmail.com>
2024-07-22 08:37:22 -0500

    created background, color and starting newsletter section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Hello!</div>
+      <div id="bg-container" className="bg-dark-slate-grey">
+        <div className="bg-white rounded-lg">
+          <div>
+            <h1 className="text-characoal-grey">Stay updated!</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

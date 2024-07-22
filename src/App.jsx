ae2c1ebda99526a6166364f831e5bdc151af79eb
Laura <laura.e.vollmer@gmail.com>
2024-07-22 08:49:18 -0500

    updating the newsletter signup function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import illustrationSignUp from "./assets/illustration-sign-up-desktop.svg";
+import Text from "./components/Text";
 
 function App() {
   return (
@@ -7,14 +8,14 @@ function App() {
       <div id="bg-container" className="bg-dark-slate-grey font-roboto">
         <div className="bg-white rounded">
           <div className="flex flex-row p-6 items-center justify-center">
-            <div className="flex">
-              <h1 className="text-characoal-grey text-2xl md:text-4xl">Stay updated!</h1>
+            <div className="flex flex-col">
+              <h1 className="text-characoal-grey text-2xl md:text-4xl">
+                Stay updated!
+              </h1>
+              <Text />
             </div>
             <div>
-              <img
-                src={illustrationSignUp}
-                alt="Illustration sign up"
-              />
+              <img src={illustrationSignUp} alt="Illustration sign up" />
             </div>
           </div>
         </div>
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,15 +3,22 @@ import iconList from "../assets/icon-list.svg";
 
 const Text = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <div>
         <h2>Join 60,000+ product managers receiving monthly updated on:</h2>
       </div>
-      <div>
-        <ul>
-          <img src={iconList} alt="icon list">
-            <li>Product discovery and building what matters</li>
-          </img>
+      <div className="space-y-2">
+        <ul className="flex flex-row">
+          <img src={iconList} alt="icon list" />
+          <li className="ml-2">Product discovery and building what matters</li>
+        </ul>
+        <ul className="flex flex-row">
+          <img src={iconList} alt="icon list" />
+          <li className="ml-2">Measuring to ensure updates are a success</li>
+        </ul>
+        <ul className="flex flex-row">
+          <img src={iconList} alt="icon list" />
+          <li className="ml-2">And much more!</li>
         </ul>
       </div>
     </div>

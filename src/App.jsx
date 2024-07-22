bc95dc6e19a4b6f2d2cc6443718589bb052681f3
Laura <laura.e.vollmer@gmail.com>
2024-07-22 08:42:06 -0500

    added roboto font in tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import illustrationSignUp from "./assets/illustration-sign-up-desktop.svg";
 
 function App() {
   return (
     <>
       <div id="bg-container" className="bg-dark-slate-grey">
         <div className="bg-white rounded-lg">
-          <div>
-            <h1 className="text-characoal-grey">Stay updated!</h1>
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex">
+              <h1 className="text-characoal-grey text-2xl md:text-4xl">Stay updated!</h1>
+            </div>
+            <div>
+              <img
+                src={illustrationSignUp}
+                className="p-6"
+                alt="Illustration sign up"
+              />
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        'tomato': "hsl(4, 100%, 67%)",
+        tomato: "hsl(4, 100%, 67%)",
         "dark-slate-grey": "hsl(234, 29%, 20%)",
         "charcoal-grey": "hsl(235, 18%, 26%)",
-        'grey': "hsl(231, 7%, 60%)",
+        grey: "hsl(231, 7%, 60%)",
         white: "hsl(0, 0%, 100%)",
       },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
+    plugins: [],
   },
-  plugins: [],
 };

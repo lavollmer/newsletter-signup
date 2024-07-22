19c66ae2321d7b719f91ab28d4c8bbfce0538619
Laura <laura.e.vollmer@gmail.com>
2024-07-22 12:29:14 -0500

    updating mobile responsive version

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Text from "./components/Text.jsx";
 import Email from "./components/Email.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ThankYou from "./components/ThankYou.jsx";
+import illustrationSignUpMobile from "./assets/illustration-sign-up-mobile.svg";
 
 function App() {
   return (
     <>
       <div id="bg-container" className="bg-dark-slate-grey font-roboto">
         <div className="bg-white rounded">
+          <img src={illustrationSignUpMobile} alt="background image" className="flex flex-col ml-4 md:hidden" />
           <div className="flex flex-row p-6 justify-center">
             <div className="flex flex-col mt-18 p-16">
               <h1 className="text-charcoal-grey font-bold text-3xl md:text-6xl">
@@ -23,7 +25,7 @@ function App() {
                 <Route path="/thank-you" element={<ThankYou />} />
               </Routes>
             </div>
-            <div>
+            <div className="hidden md:flex">
               <img
                 src={illustrationSignUp}
                 alt="Illustration sign up"

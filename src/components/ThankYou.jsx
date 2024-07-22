8c81030f90a6017d4c4c81e3b72faaeb01ac6cf1
Laura <laura.e.vollmer@gmail.com>
2024-07-22 12:38:09 -0500

    finalizing mobile application

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -8,24 +8,24 @@ const ThankYou = () => {
   return (
     <div
       id="bg-container"
-      className="md:text-charcoal-grey text-dark-slate-grey md:bg-charcoal-grey font-roboto bg-white"
+      className="mt-10 md:text-charcoal-grey text-dark-slate-grey md:bg-charcoal-grey font-roboto bg-white"
     >
       <div className="bg-white rounded-lg">
-        <div className="flex flex-col m-10">
+        <div className="flex flex-col p-4 md:m-10 ">
           <img
             src={iconSuccess}
             alt="Success icon"
-            className="h-20 w-20 ml-10"
+            className="h-20 w-20 md:ml-10"
           />
-          <h1 className="font-bold text-6xl p-10">Thanks for subscribing!</h1>
-          <p className="p-10">
+          <h1 className="font-bold text-2xl mt-6 md:text-6xl md:p-10">Thanks for subscribing!</h1>
+          <p className="flex flex-col mt-6 md:p-10">
             A confirmation email has been sent. Please open it and click the
             button inside to confirm your subscription.
           </p>
-          <div className="flex flex-col mt-40">
+          <div className="flex flex-col mt-8 md:mt-40">
             <button
               onClick={() => navigate("/")}
-              className="text-white bg-charcoal-grey rounded-lg px-10 py-4 cursor-pointer hover:bg-tomato"
+              className="text-white bg-charcoal-grey rounded-lg px-4 md:px-10 py-4 cursor-pointer hover:bg-tomato"
             >
               Dismiss message
             </button>

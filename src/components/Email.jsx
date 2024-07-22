5c2628a40c5142ba08be834045ef90b1ade44a96
Laura <laura.e.vollmer@gmail.com>
2024-07-22 11:52:37 -0500

    updating the code for successful submission

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 const Email = () => {
   const [error, setError] = useState(false);
   const validate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(false);
 
   function validateEmail(email) {
     return validate.test(email);
@@ -19,48 +21,55 @@ const Email = () => {
 
     if (validateEmail(email)) {
       setError(false);
-      onSuccess(true);
+      console.log("Email submitted: ", email);
       setEmail("");
+      setSuccess(true);
     }
   };
 
   return (
     <div className="flex flex-col mt-4">
       <h3 className="text-sm text-dark-slate-grey font-bold">Email Address</h3>
-      <form
-        id="form"
-        className="flex flex-col relative"
-        onSubmit={handleSubmit}
-      >
-        <p
-          className={`${
-            error
-              ? "text-custom-size-12 font-bold mb-2 absolute right-0"
-              : "hidden"
-          }`}
+      {success ? ( // Step 3: Conditionally render the thank you message
+        <p className="text-lg text-dark-slate-grey">Thank you for subscribing!</p>
+      ) : (
+        <form
+          id="form"
+          className="flex flex-col relative"
+          onSubmit={handleSubmit}
         >
-          Valid email required
-        </p>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          className={`${
-            error
-              ? "px-6 py-4 border-2 rounded-lg border-custom-vermellion text-custom-vermellion placeholder-custom-vermellion mb-6 bg-custom-vermellion-15 cursor-pointer"
-              : "px-6 py-4 border-2 rounded-lg border-custom-gray mb-6 cursor-pointer hover:border-custom-dark-navy"
-          }`}
-          placeholder="email@company.com"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            if (error) {
-              setError(false);
-            }
-          }}
-        />
-        <button>Subscribe to monthly newsletter</button>
-      </form>
+          <p
+            className={`${
+              error
+                ? "text-custom-size-12 font-bold mb-2 absolute right-0"
+                : "hidden"
+            }`}
+          >
+            Valid email required
+          </p>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className={`${
+              error
+                ? "px-6 py-4 border-2 rounded-lg border-custom-vermellion text-custom-vermellion placeholder-custom-vermellion mb-6 bg-custom-vermellion-15 cursor-pointer"
+                : "px-6 py-4 border-2 rounded-lg border-custom-gray mb-6 cursor-pointer hover:border-custom-dark-navy"
+            }`}
+            placeholder="email@company.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              if (error) {
+                setError(false);
+              }
+            }}
+          />
+          <button className="text-white bg-grey rounded-lg px-10 py-2">
+            Subscribe to monthly newsletter
+          </button>
+        </form>
+      )}
     </div>
   );
 };

acfa5d780d66684ca8b8cc14335b003ee59efba4
Laura <laura.e.vollmer@gmail.com>
2024-07-23 14:42:13 -0500

    updating comments on code

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const Email = () => {
   const [error, setError] = useState(false);
@@ -9,27 +8,34 @@ const Email = () => {
   const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
 
-// function named validateEmail that takes an argument email
-function validateEmail(email) {
-  // Define a regular expression for validating an email - starts with one or more charaacters that can be letters, numbers, periods, underscores, or hyphens, followed by an @ symbol, followed by one or more characters that can be letters, numbers, periods, or hyphens, followed by a period, followed by two to four letters
-  const validate = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  // Use the test method to validate the email - string matches the pattern defined by the regex
-  return validate.test(email);
-}
+  // function named validateEmail that takes an argument email
+  function validateEmail(email) {
+    // Define a regular expression for validating an email - starts with one or more charaacters that can be letters, numbers, periods, underscores, or hyphens, followed by an @ symbol, followed by one or more characters that can be letters, numbers, periods, or hyphens, followed by a period, followed by two to four letters
+    const validate = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    // Use the test method to validate the email - string matches the pattern defined by the regex
+    return validate.test(email);
+  }
 
+  // function named handleSubmit that takes an argument e that designed to handle the submission of a form within a React component
   const handleSubmit = (e) => {
+    // Prevent the default behavior of the form submission - stops the form from submitting the tradtional way
     e.preventDefault();
 
+    // The condition checks if the email variable is false or the email variable is not validated by the validateEmail function
+    // if either condition is true, set the error state to true - showing error with the form
     if (!email || !validateEmail(email)) {
       setError(true);
       return;
     }
 
+    // if the email variable is validated by the validateEmail function, set the error state to false
+    //setEmail resets the email field with an empty string
+    //navigates to thank you page
     if (validateEmail(email)) {
       setError(false);
       console.log("Email submitted: ", email);
       setEmail("");
-      navigate('/thank-you'); // Navigate to thank you page
+      navigate("/thank-you"); // Navigate to thank you page
     } else {
       setError(true);
     }
@@ -39,7 +45,9 @@ function validateEmail(email) {
     <div className="flex flex-col mt-4">
       <h3 className="text-sm text-dark-slate-grey font-bold">Email Address</h3>
       {success ? (
-        <p className="text-lg text-dark-slate-grey">Thank you for subscribing!</p>
+        <p className="text-lg text-dark-slate-grey">
+          Thank you for subscribing!
+        </p>
       ) : (
         <form
           id="form"
